16c5a735eb3bf7982ece04eeddeda2240eb4f0e2
guilhermezils <guilhermezils@icloud.com>
2021-02-16 22:40:34 -0600

    UGGGHHHH

--- Gemfile
+++ Gemfile
@@ -12,3 +12,4 @@ gem "faker"
 gem "colorize"
 gem "tty-prompt"
 gem 'activerecord-reset-pk-sequence'
+gem "rest-client", "~>2.1" 
--- app/Scheduler.rb
+++ app/Scheduler.rb
@@ -1,17 +1,29 @@
 class Scheduler
   # here will be your CLI!
-  attr_accessor :first_name, :last_name, :age, :siblings, :length_of_lesson
-  attr_reader :student_id, :teacher_id, :start_time, :name, :rate_per_hour
+  attr_accessor :age, :siblings, :length_of_lesson
+  attr_reader :first_name, :last_name, :student_id, :teacher_id, :start_time, :name, :rate_per_hour
 
   def run
-    # welcome
-    # login_or_signup
+    welcome
+    login_or_signup
     # wanna_see_favs?
     # some_method(some_argument)
     # exit
   end
 
   private
+
+  def welcome
+    puts "Welcome to the Scheduler for our School of Music"
+    sleep(1)
+  end
+
+  def login_or_signup
+    username = down_ask("Enter your username to sign up/log in:")
+    @user = User.find_or_create_by(username: username)
+  end
+
+
 
   
 end
