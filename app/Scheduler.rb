93aa628b20aadb2614fa692218a0fc6fd5858c46
guilhermezils <guilhermezils@icloud.com>
2021-02-18 14:29:42 -0600

    update helper method

--- app/Scheduler.rb
+++ app/Scheduler.rb
@@ -9,9 +9,8 @@ class Scheduler
       check_schedule
       update_length_of_lesson
       cancel_lesson
-    # wanna_see_favs?
-    # some_method(some_argument)
-    # exit
+      
+      exit
   end
 
   private
@@ -27,7 +26,7 @@ class Scheduler
   def login_or_signup
     puts"Please enter your assigned username ".colorize(:color => :red).bold
     puts "if you don'\ t have a username please write your name and contact information".colorize(:color => :red)
-    answer = gets.chomp.downcase
+    answer = STDIN.gets.chomp.downcase
     @student = Student.find_or_create_by(username: answer)
     system 'clear'
     puts "Welcome, #{@student.first_name}!".colorize(:color => :red)
@@ -43,19 +42,28 @@ class Scheduler
   def lesson_amount
     lesson_amount = Lesson.find_by(student_id)
     lesson_amount.length_of_lesson
+    #if 1.0 = 1hour
+    #if 0.5 - 30min
   end
+#helper method
+  def updated_schedule
+    x = @student.id
+    update = Lesson.find_by(student_id:x)
+    update.length_of_lesson
+  end
+
 
   
   
   def check_schedule
     puts " \nWoud you like to check your schedule? Y/N".colorize(:color => :red)
-    answer = gets.chomp.downcase
+    answer = STDIN.gets.chomp.downcase
     if answer.downcase == "y"
       system 'clear'
       #helper method
-     puts student_schedule
-     puts lesson_amount
-     sleep(1)
+      puts student_schedule
+      puts lesson_amount
+      sleep(1)
     else
       system 'clear'
       puts "Thanks".colorize(:color => :red)
@@ -67,45 +75,50 @@ class Scheduler
 
   def update_length_of_lesson
     puts "\nWould you like to change length of lesson? Y/N".colorize(:color => :red)
-    answer = gets.chomp.downcase
+    answer = STDIN.gets.chomp.downcase
   
     if answer.downcase == "y"
       system 'clear'
       puts "\nHow long would you like? \n \n 0.5 = 30min \n 1.0 = 1hour".colorize(:color => :red)
-      num = gets.chomp
+      num = STDIN.gets.chomp
 
       x = @student.id
-    Lesson.where(student_id:x).update(length_of_lesson: num.to_f)
+      Lesson.where(student_id:x).update(length_of_lesson: num.to_f)
+      #puts updated_schedule
   
     else
-  puts "\nGreat".colorize(:color => :red)
+      puts "\nGreat".colorize(:color => :red)
+      system 'clear'
     end
-
+    
   end
 
   def cancel_lesson
     puts "Would you like to cancel your lesson? Y/N".colorize(:color => :red)
-    answer = gets.chomp.downcase
+    answer = STDIN.gets.chomp.downcase
     if answer.downcase == "y"
       puts "Are you sure?".colorize(:color => :red)
-      answer = gets.chomp.downcase
+      answer = STDIN.gets.chomp.downcase
       if answer.downcase == "y"
         x = @student.id
         Lesson.where(student_id:x).destroy
       end
-    else
-      system 'clear'
-      sleep(1)
-      puts "Thanks, have a nice day!".colorize(:color => :red)
-
-      sleep(5)
       
     end
 
   end
 
+
   
-  
+  def exit
+    
+    system 'clear'
+    sleep(1)
+    puts "Thanks, have a nice day!".colorize(:color => :red)
+
+    sleep(3)
+    
+  end
 
 
   
